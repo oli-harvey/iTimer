6ded898a3f963720b34503361a4920c67fca7d78
Oli Harvey <oliharvey@hotmail.com>
2023-08-13 08:18:34 +0100

    Initial Commit

---
+++ iTimer/iTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTimerApp.swift
+//  iTimer
+//
+//  Created by Oliver Harvey on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct iTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
