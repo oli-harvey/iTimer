1cd7454d0ca8c8e5c7fd5f860e595cf8a131d435
Oli Harvey <oliharvey@hotmail.com>
2023-09-14 08:21:09 +0100

    Improved dots

--- iTimer/View/TimerView.swift
+++ iTimer/View/TimerView.swift
@@ -7,7 +7,7 @@ struct TimerView: View {
     @State private var isIntervalCompleted = false
     
     @Environment(\.presentationMode) var presentationMode
-    let dotSize: CGFloat = 10
+    let dotSize: CGFloat = 8
     private let buttonSize = ScreenSize.deviceWidth * 0.2
     private let circleSize = ScreenSize.deviceWidth * 0.65
 
@@ -113,32 +113,92 @@ struct TimerNumericalView: View {
     }
 }
 
+//struct IntervalDotsView: View {
+//    @ObservedObject var timer: IntervalTimer
+//    @Binding var isIntervalCompleted: Bool
+//    let dotSize: CGFloat
+//
+//    var body: some View {
+//        LazyVGrid(columns: Array(repeating:GridItem(.fixed(dotSize)), count: min(10, timer.totalIntervals)), spacing: 10) {
+//            ForEach(0..<timer.totalIntervals, id:\.self) { intervalIndex in
+//                Circle()
+//                    .fill(timer.intervalsElapsed > intervalIndex || isIntervalCompleted  ? Color.orange : Color.gray)
+////                            .frame(width: 10, height: 10)
+//                    .scaleEffect(isIntervalCompleted ? 1.2 : 1.0)
+//                    .animation(.easeInOut(duration: 0.2), value: isIntervalCompleted)
+//                    .onReceive(timer.$intervalsElapsed) { _ in
+//                        // Trigger a flash animation when an interval is completed
+//                        withAnimation(.easeInOut(duration: 0.2)) {
+//                            isIntervalCompleted = true
+//                        }
+//                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+//                            withAnimation(.easeInOut(duration: 0.2)) {
+//                                isIntervalCompleted = false
+//                            }
+//                        }
+//                    }
+//            }
+//        }
+//    }
+//}
 struct IntervalDotsView: View {
     @ObservedObject var timer: IntervalTimer
     @Binding var isIntervalCompleted: Bool
     let dotSize: CGFloat
 
+    // Constants
+    let maxDotsPerRow = 6
+    let dotSpacing: CGFloat = 4
+    let maxDotsToShowText = 18
+
     var body: some View {
-        LazyVGrid(columns: Array(repeating:GridItem(.fixed(dotSize)), count: min(10, timer.totalIntervals)), spacing: 10) {
-            ForEach(0..<timer.totalIntervals, id:\.self) { intervalIndex in
-                Circle()
-                    .fill(timer.intervalsElapsed > intervalIndex || isIntervalCompleted  ? Color.orange : Color.gray)
-//                            .frame(width: 10, height: 10)
-                    .scaleEffect(isIntervalCompleted ? 1.2 : 1.0)
-                    .animation(.easeInOut(duration: 0.2), value: isIntervalCompleted)
-                    .onReceive(timer.$intervalsElapsed) { _ in
-                        // Trigger a flash animation when an interval is completed
-                        withAnimation(.easeInOut(duration: 0.2)) {
-                            isIntervalCompleted = true
-                        }
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                            withAnimation(.easeInOut(duration: 0.2)) {
-                                isIntervalCompleted = false
+        let totalIntervals = timer.totalIntervals
+        let numberOfRows = (totalIntervals + maxDotsPerRow - 1) / maxDotsPerRow
+
+        let intervalsToDisplay = min(totalIntervals, maxDotsToShowText)
+        let extraDotsCount = totalIntervals - maxDotsToShowText
+
+        VStack {
+            if totalIntervals > maxDotsToShowText {
+                HStack {
+                    Circle()
+                        .fill(Color.orange)
+                        .frame(width: dotSize, height: dotSize)
+                    Text("\(timer.intervalsElapsed) / \(totalIntervals)")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                }
+                .frame(maxWidth: .infinity)
+                .frame(height: dotSize + 10) // Adjust height as needed
+            } else {
+                ForEach(0..<numberOfRows, id: \.self) { rowIndex in
+                    HStack(spacing: dotSpacing) {
+                        ForEach(0..<maxDotsPerRow, id: \.self) { columnIndex in
+                            let dotIndex = rowIndex * maxDotsPerRow + columnIndex
+                            if dotIndex < intervalsToDisplay {
+                                Circle()
+                                    .fill(timer.intervalsElapsed > dotIndex || isIntervalCompleted ? Color.orange : Color.gray)
+                                    .frame(width: dotSize, height: dotSize)
+                                    .scaleEffect(isIntervalCompleted ? 1.2 : 1.0)
+                                    .animation(.easeInOut(duration: 0.2), value: isIntervalCompleted)
+                                    .onReceive(timer.$intervalsElapsed) { _ in
+                                        // Trigger a flash animation when an interval is completed
+                                        withAnimation(.easeInOut(duration: 0.2)) {
+                                            isIntervalCompleted = true
+                                        }
+                                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                            withAnimation(.easeInOut(duration: 0.2)) {
+                                                isIntervalCompleted = false
+                                            }
+                                        }
+                                    }
                             }
                         }
                     }
+                }
             }
         }
+        .frame(maxWidth: .infinity)
+        .frame(height: totalIntervals > maxDotsToShowText ? dotSize + 10 : CGFloat(numberOfRows) * (dotSize + dotSpacing))
     }
 }
-
